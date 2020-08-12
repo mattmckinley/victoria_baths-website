61ef3f13cad0b53c8d21c44abdf2d9a86ba7a00e
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-08-12 15:38:21 +0100

    history page layout

--- src/History/History.js
+++ src/History/History.js
@@ -1,12 +1,363 @@
 import React from 'react';
 import './History.css';
 
+import History1 from './Images/m77945.jpg';
+import History2 from './Images/m779541.jpg';
+import History3 from './Images/m51825.jpg';
+import History4 from './Images/m51814.jpg';
+import History5 from './Images/m5.jpg';
+import History6 from './Images/m6.jpeg';
+import History7 from './Images/m7.jpg';
+import History8 from './Images/m8.jpg';
+import History9 from './Images/m9.jpg';
+import History10 from './Images/m10.jpg';
+import History11 from './Images/m11.jpg';
+import History12 from './Images/m12.jpg';
+import History13 from './Images/m13.jpg';
+import History14 from './Images/m14.jpg';
+import History15 from './Images/m15.jpg';
+import History16 from './Images/m16.jpg';
+import History17 from './Images/m17.jpg';
+import History18 from './Images/m18.jpg';
+
+
 function History() {
   return (
-    <div className="history-main">
-        <div>
-            <h1>History</h1>
+    <div className="container history-main">
+      <div className="row">
+        <div className="col col-md-12 history-title">
+          <h1>History Timeline</h1>
         </div>
+      </div>
+      <div className="row">
+        <div className="col col-md-12 history-intro_par">
+          <p>When it opened in 1906, Victoria Baths on Hathersage Road, Manchester, was described as "the most splendid
+          municipal bathing institution in the country" and "a water palace of which every citizen of Manchester can be proud."
+          Not only did the building provide spacious and extensive facilities for swimming, bathing and leisure, it was built of
+          the highest quality materials with many period decorative features:- stained glass, terracotta, tiles and mosaic floors.
+          Victoria Baths served the people of central Manchester for 87 years and established themselves in the affections of all
+          those who used the facilities.</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1897</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Planning the build</h2>
+          </div>
+          <div className="history-section_par">
+            <p>The plan to provide Baths to serve the three wards of Longsight, St Luke's and Rusholme was first considered in 1897
+            by the Baths and Wash-houses Committee of Manchester Corporation and in 1899 the site was purchased.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History1} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1902</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Under construction</h2>
+          </div>
+          <div className="history-section_par">
+            <p>In 1902 Mr Henry Price was appointed as the first City Architect of Manchester and became responsible
+            for the Victoria Baths building project. Building commenced in 1903 and a commemorative terracotta coat of
+              arms was installed in the Males 1st Class / Gala pool.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History2} alt="building under construction" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1906</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Completion</h2>
+          </div>
+          <div className="history-section_par">
+            <p>By the time the Baths had been completed, the cost had risen to over £59,000, more than double the usual
+            price of building a public baths. But the council were clear in their justification for the expenditure -
+              they had indeed created a "water palace"</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History3} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1906</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>State of the art plant</h2>
+          </div>
+          <div className="history-section_par">
+            <p>The Galloway boilers were installed in 1906 along with the sand filters for filtering the well water.
+            These boilers produced the hot water and heating for the building and were fired by stokers who shovelled
+               the coal by hand into the furnaces.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History4} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1906</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Turkish Baths Suite</h2>
+          </div>
+          <div className="history-section_par">
+            <p>The Turkish Baths suite at Victoria Baths was one of several built by Manchester Corporation.
+            It contained three hot-rooms, a shampooing room, a douche (shower), Russian bath (steam room) and a Rest room.
+              Lots of info on Victorian Turkish Baths from www.victorianturkishbath.org</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History5} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1908</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Water Polo and Swimming</h2>
+          </div>
+          <div className="history-section_par">
+            <p>Manchester boasted many leading swimmers in the early 1900's including Rob Derbyshire who lived at
+            Victoria Baths as his father was the superintendent. Rob was a very successful swimmer and water polo player,
+              competing in four Olympic Games.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History6} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1922</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Mixed Bathing</h2>
+          </div>
+          <div className="history-section_par">
+            <p>Mixed bathing was introduced in Manchester with great caution in 1914, and by the 1920s sessions
+              at Victoria Baths were held every Sunday morning enabling families to swim together for the first time.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History7} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1930</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Progress in Water Treatment</h2>
+          </div>
+          <div className="history-section_par">
+            <p>The treatment of pool water progressed over the years and aerators were introduced to freshen up the
+            water so that it could be re-used. By 1930 Victoria Baths had a chlorination plant which controlled the
+              bacteria in the water.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History8} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1933</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Sunny Lowry swims the Channel</h2>
+          </div>
+          <div className="history-section_par">
+            <p>The Channel swimmer Sunny Lowry (b.1911 - d.2008) began her swimming career at Victoria Baths.
+            She succeeded in swimming the channel on her third attempt in August 1933, becoming the fifth British
+              woman to swim the channel.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History9} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1934</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>John Besford wins the Eagle Trophy</h2>
+          </div>
+          <div className="history-section_par">
+            <p>The Olympic swimmer John Besford trained at Victoria Baths. Besford infuriated Hitler by winning the 1934
+            European 100 backstroke title. Hitler had commissioned a magnificent 1cwt bronze eagle trophy on the assumption
+              that German favourite, Ernst Kuppers, would win.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History10} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1938</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Zilpha Grant travels to Australia</h2>
+          </div>
+          <div className="history-section_par">
+            <p>Zilpha Grant first swam at Victoria Baths with her school, St Paul's, Withington. She was
+            taught by Mrs Eleanor Redfearn and encouraged to join South Manchester Swimming Club. Zilpha went
+              on to win a silver medal in the freestyle relay at the 1938 Empire Games, Australia.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History11} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1946</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Dance</h2>
+          </div>
+          <div className="history-section_par">
+            <p>The Gala Pool was floored over during the winter months and used as a venue for dances, particularly in
+              the post war years. In the 1950s music was provided by the likes of Percy Pease, Phil Moss and other local bands.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History12} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1947</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Courtship</h2>
+          </div>
+          <div className="history-section_par">
+            <p>Like so many other romances that began at Victoria Baths, Beatrice Hooley and Bill Ramsden met on a
+            blind date at one of the popular dances. The courtship blossomed, they married and went on to have a
+              beautiful baby boy, Russell.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History13} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1950</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Bowling at the Baths</h2>
+          </div>
+          <div className="history-section_par">
+            <p>In the 1950s, bowls became a popular activity, making use of the Males 1st Class / Gala pool
+              when it was covered over in the winter months.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History14} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1952</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>The Botham family</h2>
+          </div>
+          <div className="history-section_par">
+            <p>Mr Frank Botham was the Superintendent at Victoria Baths between 1945 - 1960.
+            Roy, his eldest son, and Jean, his youngest daughter, were the first brother and sister to
+              swim in the same Olympic Games in Helsinki, 1952.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History15} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1986</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Sports hall conversion</h2>
+          </div>
+          <div className="history-section_par">
+            <p>The Council renovated the Males 2nd Class pool and converted it into a sports hall. The pool was
+              covered over and then used for five-aside-football, badminton, netball and basketball.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History16} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1991</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Swimming World Champion</h2>
+          </div>
+          <div className="history-section_par">
+            <p>James Hickman has had a long and successful swimming career spanning three Olympics,
+            three Commonwealth Games and many World Championships. The Bax Shield that he won in 1991
+              is held in the Victoria Baths Archive. James is a patron of the Friends of Victoria Baths.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History17} alt="building plans" />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12 col-md-2 history-section_year">
+          <h2>1993</h2>
+        </div>
+        <div className="col-12 col-md-4 history-section_text">
+          <div className="history-section_title">
+            <h2>Campaign against closure</h2>
+          </div>
+          <div className="history-section_par">
+            <p>Local residents tried to prevent the closure of Victoria Baths but the council couldn't justify
+            the expense of keeping it open. So the building closed on 13th March 1993. The Friends and the Trust
+              were formally constituted later that year.</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-6 history-year-image">
+          <img src={History18} alt="building plans" />
+        </div>
+      </div>
     </div>
   );
 }
