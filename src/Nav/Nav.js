ca3c3bde594528ef44183027c5dd0444faaa0bf5
Matt Mckinley <matt.g.mckinley@gmail.com>
2020-08-12 13:56:12 +0100

    bootstrap added, Navbar update

--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -6,24 +6,19 @@ import './Nav.css';
 function Nav() {
     return (
         <div className="nav-main">
-            <nav className="navbar">
-                <ul className="nav-list">
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/Visit">Visit</Link>
-                    </li>
-                    <li>
-                        <Link to="/History">History</Link>
-                    </li>
-                    <li>
-                        <Link to="/Support">Support</Link>
-                    </li>
-                    <li>
-                        <Link to="/Hire">Hire</Link>
-                    </li>
-                </ul>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="navbar-brand"><Link to="/">Victoria Baths</Link></div>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <div className="nav-link"><Link to="/Visit">Visit</Link></div>
+                        <div className="nav-link"><Link to="/History">History</Link></div>
+                        <div className="nav-link"><Link to="/Support">Support</Link></div>
+                        <div className="nav-link"><Link to="/Hire">Hire</Link></div>
+                    </div>
+                </div>
             </nav>
         </div>
     );
